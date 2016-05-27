7c5d7259fdba92a05433a993f0ed60c4e62dadf1
Merzkiy <skazo4nix@gmail.com>
2016-05-27 10:51:15 +0300

    chapter8

--- app/src/main/java/com/spark/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/spark/criminalintent/CrimeActivity.java
@@ -13,12 +13,12 @@ public class CrimeActivity extends FragmentActivity {
         setContentView(R.layout.activity_crime);
 
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if(fragment == null){
             fragment = new CrimeFragment();
             fm.beginTransaction()
-                    .add(R.id.fragmentContainer, fragment)
+                    .add(R.id.fragment_container, fragment)
                     .commit();
         }
     }
